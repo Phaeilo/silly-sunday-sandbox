11092aa5809bd3c2fdc1d1e0124c2e2d12de36ad
Philip Huppert <philip@zeilen-sprung.de>
2026-04-14 00:45:09 +0200

    Rename hook

--- run-container.sh
+++ run-container.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 jq --arg dir "$PWD" '.hook.path = $dir + "/firewall.sh"' \
-    "$PWD/hooks/iptables_hook.json" > "$PWD/hooks/iptables_hook.json.tmp" \
-    && mv "$PWD/hooks/iptables_hook.json.tmp" "$PWD/hooks/iptables_hook.json"
+    "$PWD/hooks/firewall_hook.json" > "$PWD/hooks/firewall_hook.json.tmp" \
+    && mv "$PWD/hooks/firewall_hook.json.tmp" "$PWD/hooks/firewall_hook.json"
 
 podman \
     --hooks-dir "$PWD/hooks" \
     run --rm -it \
-    --annotation myannotation=yes \
+    --annotation firewall=yes \
     --network "pasta:--address,10.0.2.100,--netmask,255.255.255.0,--gateway,10.0.2.2,--map-host-loopback,10.0.2.2,--no-udp,--no-icmp,--no-ndp,--no-dhcpv6,--no-ra,--ipv4-only" \
     --volume ./shared:/shared \
     --volume /usr/lib/kitty/terminfo/x/xterm-kitty:/home/ubuntu/.terminfo/x/xterm-kitty:ro \
